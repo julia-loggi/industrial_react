9bc2a0a4869838da010ffbfde456f9d24109dce9
JulieSemenova <julie.logg@gmail.com>
2017-08-14 18:13:53 +0300

    build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import indLogo from './logo.svg';
-import './Style.css';
-import searchLogo from './img/search.svg'
-import product1 from './img/product-1.jpg'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./Style.css";
+import searchLogo from "./img/search.svg"
 
 class Content extends Component {
   render() {
@@ -19,163 +18,248 @@ class Content extends Component {
 
 export default Content;
 
-function Header() {
-  return (
-    <header className="page-header">
-      <img src={indLogo} className="page-header__logo" alt="logo" />
-      <SearchForm />
-    </header>
-  );
+
+class Header extends Component {
+  render () {
+    return (
+      <header className="page-header">
+        <img src={logo} className="page-header__logo" alt="logo" />
+        <SearchForm />
+      </header>
+    );
+  }
 }
 
-function SearchForm() {
-  return (
-    <form 
-      className='page-header__search'
-      action="#" 
-      method="get"
-    >
-      <div className="field-text">
-        <label className="field-text__item">
-          <img className="field-text__icon" src={searchLogo} />
-          <input
-            className="field-text__input"
-            type="search"
-            name="search" 
-            placeholder="Search"
-          />
-        </label>
-      </div>
-    </form>
-  );
-}
-
-function Subscribe() {
-  return (
-    <div className="subscribe">
-      <h3 className="subscribe__title">
-        Get our freshest content delivered to your inbox
-      </h3>
-      <p className="subscribe__text">
-        For the latest stories, news, events, promotions and exclusive offers, sign up today. We know you get a lot of emails, but we promise...ours is the one you want.
-      </p>
-      <form className="subscribe__form" action="#" method="post">
-        <div className="field-text field-text--email">
+class SearchForm extends Component {
+  render () { 
+    return (
+      <form 
+        className="page-header__search"
+        action="#" 
+        method="get"
+      >
+        <div className="field-text">
           <label className="field-text__item">
-            <input 
-              className="field-text__input" 
-              type="email" 
-              name="email" 
-              autocomplete="on" 
-              placeholder="Your email"
-              />
+            <img className="field-text__icon" src={searchLogo} alt="search" />
+            <input
+              className="field-text__input"
+              type="search"
+              name="search" 
+              placeholder="Search"
+            />
           </label>
         </div>
-        <input 
-          className="subscribe__btn btn" 
-          type="submit" 
-          value="Subscribe"
-        />
       </form>
-    </div>
-  );
+    );
+}
 }
 
-function Main() {
-  return (
-    <main className="main">
-      <Catalog data={catalogItems}/>
-      <Subscribe />
-    </main>
-  );
+class Subscribe extends Component  {
+  render () {
+    return (
+      <div className="subscribe">
+        <h3 className="subscribe__title">
+          Get our freshest content delivered to your inbox
+        </h3>
+        <p className="subscribe__text">
+          For the latest stories, news, events, promotions and exclusive offers, sign up today. We know you get a lot of emails, but we promise...ours is the one you want.
+        </p>
+        <form className="subscribe__form" action="#" method="post">
+          <div className="field-text field-text--email">
+            <label className="field-text__item">
+              <input 
+                className="field-text__input" 
+                type="email" 
+                name="email" 
+                autoComplete="on" 
+                placeholder="Your email"
+                />
+            </label>
+          </div>
+          <input 
+            className="subscribe__btn btn" 
+            type="submit" 
+            value="Subscribe"
+          />
+        </form>
+      </div>
+    );
+  }
 }
+
+class Main extends Component {
+  render () {
+    return (
+      <main className="main">
+        <Catalog data={catalogItems} />
+        <Subscribe />
+      </main>
+    );
+  }
+}
+
 var catalogItems = [
   {
-    name: 'Gina',
-    description: 'A coffee instrument combining classic coffee techniques with the latest technology',
-    btn: 'Shop Now',
-    index: '1',
-    size: 'large'
+    name: "Gina",
+    description: "A coffee instrument combining classic coffee techniques with the latest technology",
+    moreAbout: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.",
+    btn: "Shop Now",
+    index: "1",
+    size: "large"
   },
   {
-    name: 'Beoplay A1',
-    description: 'The ultra-portable Bluetooth speaker that will travel with you anywhere',
-    btn: 'buy',
-    index: '2',
-    size: 'medium'
+    name: "Beoplay A1",
+    description: "The ultra-portable Bluetooth speaker that will travel with you anywhere",
+    moreAbout: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.",
+    btn: "buy",
+    index: "2",
+    size: "medium"
   },
   {
-    name: 'Leica MP',
-    description: 'Focused technology for focused photography',
-    btn: 'buy',
-    index: '3',
-    size: 'medium'
+    name: "Leica MP",
+    description: "Focused technology for focused photography",
+    moreAbout: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.",
+    btn: "buy",
+    index: "3",
+    size: "medium"
   },
   {
-    name: 'Molekule',
-    description: 'The world\'s first molecular air purifier',
-    btn: 'details',
-    index: '4',
-    size: 'small'
+    name: "Molekule",
+    description: "The world\"s first molecular air purifier",
+    moreAbout: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.",
+    btn: "details",
+    index: "4",
+    size: "small"
   },
   {
-    name: 'P9 Signature',
-    description: 'Bowers & Wilkins',
-    btn: 'details',
-    index: '5',
-    size: 'small'
+    name: "P9 Signature",
+    description: "Bowers & Wilkins",
+    moreAbout: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.",
+    btn: "details",
+    index: "5",
+    size: "small"
   },
   {
-    name: 'DR-II',
-    description: 'An homage to Dieter Rams',
-    btn: 'details',
-    index: '6',
-    size: 'small'
+    name: "DR-II",
+    description: "An homage to Dieter Rams",
+    moreAbout: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus. Curabitur blandit tempus porttitor.",
+    btn: "details",
+    index: "6",
+    size: "small"
   }
 ];
 
-var Catalog = React.createClass ({
-  render: function() {
+class Item extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {visible: false};
+    this.readmoreClick = this.readmoreClick.bind(this);
+    this.closeClick = this.closeClick.bind(this);
+  }
+  
+  readmoreClick() {
+    this.setState(prevState => ({
+      visible: !prevState.visible
+    }));
+  }
+  
+  closeClick() {
+    this.setState(prevState => ({
+      visible: !prevState.visible
+    }));
+  }
+  
+  render () {
+    var name = this.props.data.name,
+        description = this.props.data.description,
+        moreAbout = this.props.data.moreAbout,
+        btn = this.props.data.btn,
+        index = this.props.data.index,
+        size = this.props.data.size,
+        visible = this.state.visible;
+  
+    return (
+      <section className={"catalog-item catalog-item__item-"+index + " catalog-item--"+size} key={index}>
+        <h2 className="catalog-item__title">{name}</h2>
+        <p className="catalog-item__desc">{description}</p>
+        <a
+          onClick={this.readmoreClick}
+          className={"catalog-item__btn btn btn--"+btn} 
+        >
+          {btn}
+        </a>
+        <div className={"catalog-item__overlay "+ (visible ? "" : "none")}>
+          <p className="catalog-item__info">
+            {moreAbout}
+          </p>
+          <button 
+            onClick={this.closeClick}
+            className="catalog-item__close-btn"
+          >
+            Close
+          </button>
+        </div>
+      </section>
+    );
+  }
+}
+
+class Catalog extends Component {
+  render() {
     var data = this.props.data;
-    var itemsTemplate = data.map(function(item, index) {
+    var itemsTemplate;
+  
+    itemsTemplate = data.map(function(item, index) {
       return (
-        <section className={"catalog-item catalog-item__item-"+item.index + " catalog-item--"+item.size} key={item.index}>
-          <h2 className="catalog-item__title">{item.name}</h2>
-          <p className="catalog-item__desc">{item.description}</p>
-          <a className={"catalog-item__btn btn btn--"+item.btn} href="#">{item.btn}</a>
-        </section>
-      )
+          <Item data={item} key={index}/>
+      );
     })
     
     return (
-    <div className="catalog">
-      {itemsTemplate}
-    </div>
+      <div className="catalog">
+        {itemsTemplate}
+      </div>
     );
-  }
-});
-
-
-
-function Footer() {
-  return (
-    <footer className="page-footer">
-      <SocialLinks />
-      <address className="page-footer__copyright">
-        &#169 Industrial Design Magazine
-      </address>
-    </footer>
-  );
+  } 
 }
 
-function SocialLinks() {
+
+class Footer extends Component {
+  render () {  
+    return (
+      <footer className="page-footer">
+        <SocialLinks socialLinks={socialLinks} />
+        <address className="page-footer__copyright">
+        {String.fromCharCode(169) + " Industrial Design Magazine"}
+        </address>
+      </footer>
+    );
+  }
+}
+
+const socialLinks = ['About', 'Search', 'Contact', 'Facebook', 'Twitter'];
+
+function SocialLinks(props) {
+  const socialLinks = props.socialLinks;
+  const listItems = socialLinks.map((socialLinks) => 
+    <li key={socialLinks.toString()} className="links-list__item">
+      <a className="links-list__link">
+        {socialLinks}
+      </a>
+    </li>
+  );
+  
   return (
     <section className="page-footer__links">
+      <ul className="links-list">
+        {listItems}
+      </ul>
     </section>
   );
+
 }
 
 ReactDOM.render (
   <Content />,
-  document.getElementById('container')
+  document.getElementById("container")
 );
